80f1ab3750ca3dcad43652c1a61f5ff170bfefb0
rudrakshberde <94887735+rudrakshberde@users.noreply.github.com>
2025-09-28 23:10:02 -0700

    setting up context

--- src/contexts/FileContexts.js
+++ src/contexts/FileContexts.js
@@ -1,3 +1,14 @@
-import { createContext } from "react"
+import { createContext, useState, useContext, Children } from "react"
 
-export const FileContext = ()=> createContext()
+ export const FileContext = ()=> createContext();
+ 
+ export const  FileContextProvider = ()=>{
+    Const [CertBase64,SetcertBase64]=useState(null);
+    return (
+        <FileContext.Provider  value={{CertBase64,SetcertBase64}}>
+        {Children}
+        </FileContext.Provider>
+    );
+
+
+ }
